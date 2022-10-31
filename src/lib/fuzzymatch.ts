9d9d95a91c79924262f7eabb484e290246d3f987
JonathanColetti <88168630+JonathanColetti@users.noreply.github.com>
2022-10-30 21:09:21 -0400

    Create fuzzymatch.ts

---
+++ src/lib/fuzzymatch.ts
@@ -0,0 +1,9 @@
+export default function fuzzymatch(tomatch: string,fquery: string) {
+    tomatch = tomatch.toLowerCase()
+    fquery = fquery.toLowerCase()
+    let i = 0, n = -1, l;
+    for (; l = fquery[i++] ;) {
+        if (!~(n = tomatch.indexOf(l, n+1))) return false
+    }
+    return true;
+}
