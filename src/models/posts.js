d73f967dcc327887f8ac8e7893af328a940413c8
JonathanColetti <88168630+JonathanColetti@users.noreply.github.com>
2022-10-30 21:05:13 -0400

    Create posts.js

---
+++ src/models/posts.js
@@ -0,0 +1,80 @@
+import {Sequelize} from 'sequelize'; export default function(sequelize, DataTypes) {
+  return sequelize.define('posts', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    ctid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'mediatype',
+        key: 'id'
+      }
+    },
+    pid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'profiles',
+        key: 'id'
+      }
+    },
+    caption: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    medialnk: {
+      type: DataTypes.STRING(500),
+      allowNull: true
+    },
+    plocation: {
+      type: DataTypes.STRING(200),
+      allowNull: true
+    },
+    pinned: {
+      type: DataTypes.TINYINT,
+      allowNull: true,
+      defaultValue: 0
+    },
+    time_created: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    time_updated: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
+    }
+  }, {
+    sequelize,
+    tableName: 'posts',
+    timestamps: true,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+      {
+        name: "ctid",
+        using: "BTREE",
+        fields: [
+          { name: "ctid" },
+        ]
+      },
+      {
+        name: "pid",
+        using: "BTREE",
+        fields: [
+          { name: "pid" },
+        ]
+      },
+    ]
+  });
+};
