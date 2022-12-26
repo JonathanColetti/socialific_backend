507499729e3e05fcd51909672de64b68af26271a
Jonathan Coletti <jonathan.colettiroot@gmail.com>
2022-12-25 19:25:49 -0500

    fix db, more mutations

--- src/mutations/mposts.ts
+++ src/mutations/mposts.ts
@@ -4,7 +4,8 @@ import { Irepoting } from "../lib/util/interfaces/reports";
 import checkvalid from "../lib/util/verification/checkvalid";
 import { MissingError } from "../reporting/rdb";
 import { iuserauth } from "../resolvers/ruserauth";
-
+import mmediatype from "./mmediatype";
+import keyword_extractor from "keyword-extractor";
 
 export default async function mpost(_arguments: IPostsInput, ip: string) {
     if ((!checkvalid(_arguments.uid)) && (!checkvalid(_arguments.caption) || !checkvalid(_arguments.medialnk)) ) {
@@ -24,19 +25,37 @@ export default async function mpost(_arguments: IPostsInput, ip: string) {
             userid: _arguments.uid
         }
     });
-    // if (theauth === null) {console.log("AUTH NULL")}
-    console.log(_arguments.ctid)
-    const thepost = db.posts.create({
+    if (theauth === null) {console.log("AUTH NULL")}
+    
+
+    const thepost = await db.posts.create({
         auid:  theauth.id,
-        ctid: _arguments.ctid,
         soundid: _arguments.soundid,
         caption: _arguments.caption,
         plocation: _arguments.plocation,
         medialnk: _arguments.medialnk,
+        whosees: _arguments.whosees
     }).catch((err: any) => {
         console.log(err)
         return {state: err.message, post: null}
     })
+    if (_arguments.whosees !== "all") {
+        
+    }
+    const keywords: string[] = keyword_extractor.extract(_arguments.caption, {
+        language: "english",
+        remove_digits: false,
+        return_chained_words: false,
+        remove_duplicates: true,
+        return_changed_case: true,
+    })
+    // try to make new media types
+    // add mediatype and postid to relationdb
+    keywords.forEach(async element => {
+        await mmediatype({uid: _arguments.uid, name: element}, ip);
+        
+    });
+
     return {
         state: "Sucess",
         post: thepost
