3eca60a972885088eab8effad17e9538d0ed9bfb
Jonathan Coletti <jonathan.colettiroot@gmail.com>
2022-12-28 19:56:27 -0500

    code cleaning

--- src/lib/util/interfaces/inputs.ts
+++ src/lib/util/interfaces/inputs.ts
@@ -25,6 +25,7 @@ export interface IProfileInput extends Input {
     name: string 
     font: string 
     location: string
+    pubbg: string
 }
 
 export interface IUauthInput extends Input {
@@ -40,6 +41,30 @@ export interface ICommentsInput extends Input {
     comment: string
     medialnk: string
 }
-export interface IMediatype extends Input {
-    name: string
+
+export interface IRuserauth extends Input {
+    email: string
+    phonenum: string
+    password: string
 }
+
+export interface IRprofiles extends Input {
+    username: string
+    id: number
+}
+
+export interface IRposts extends Input {
+    tid: number
+    pid: number
+    sndid: number
+    mid: number
+}
+
+export interface IRcomments extends Input {postid: number}
+
+export interface IMediatype extends Input {name: string}
+
+export interface IPostactions extends Input {postid: number}
+
+export interface IProfileactions extends Input {profileid: number}
+
